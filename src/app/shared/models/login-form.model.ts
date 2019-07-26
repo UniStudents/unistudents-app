5c837656a52334764dbef93b640cfe18192a056a
nikos.skl <nikos.skl98@gmail.com>
2019-07-26 19:22:54 +0300

    Add login form model and http post method

---
+++ src/app/shared/models/login-form.model.ts
@@ -0,0 +1,4 @@
+export class LoginForm {
+    username: string;
+    password: string;
+}
--- src/app/shared/services/generic-http.service.ts
+++ src/app/shared/services/generic-http.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {LoginForm} from '../models/login-form.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenericHttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  apiUrl = 'http://192.168.3.116:8080/api/grades';
+
+  loginForm: LoginForm = {
+    username: 'username',
+    password: 'password'
+  };
+
+  getGradeResults() {
+    return this.http.post(this.apiUrl, this.loginForm);
+  }
 }
