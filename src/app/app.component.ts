30bb4d93fffa80344f269c862699a8461b96aebc
nikos.skl <nikos.skl98@gmail.com>
2019-07-26 17:25:39 +0300

    Change grades design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.styleLightContent();
       this.splashScreen.hide();
     });
   }
