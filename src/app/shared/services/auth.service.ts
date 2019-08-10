fb3880216e1d4d197faf242cf7aed75ee6edf2dd
nikos.skl <nikos.skl98@gmail.com>
2019-08-10 19:49:24 +0300

    Add authentication service class

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Storage} from '@ionic/storage';
+import {EnvService} from './env.service';
+import {LoginForm} from '../models/login-form.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isLoggedIn = false;
+
+  constructor(
+    private http: HttpClient,
+    private storage: Storage,
+    private env: EnvService
+  ) { }
+
+  login(username, password) {
+    return this.http.post(this.env.API_URL + '/api/grades', {
+      username: username,
+      password: password
+    });
+  }
+
+  logout() {
+    this.storage.remove('userData');
+    this.isLoggedIn = false;
+  }
+}
