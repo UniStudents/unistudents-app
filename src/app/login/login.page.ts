a36fa0489f012ced43285aa135a93a89bb88850c
nikos.skl <nikos.skl98@gmail.com>
2019-08-10 19:51:18 +0300

    Add login form and submit action

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {AuthService} from '../shared/services/auth.service';
+import {LoadingController} from '@ionic/angular';
+import {Storage} from '@ionic/storage';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(
+      private router: Router,
+      private storage: Storage,
+      private authService: AuthService,
+      public loadingController: LoadingController
+  ) { }
 
   ngOnInit() {
   }
+
+  async login(form: NgForm) {
+
+    const loading = await this.loadingController.create({
+      message: 'Please wait..'
+    });
+
+    await loading.present();
+
+    this.authService.login(form.value.username, form.value.password)
+        .subscribe(
+            (response) => {
+              this.storage.set('userData', response);
+              this.authService.isLoggedIn = true;
+              this.router.navigate(['/app/tabs/tab1']);
+            },
+            (error) => {
+              if (error.status === 401) {
+              } else {
+              }
+              loading.dismiss();
+            },
+            () => {
+              loading.dismiss();
+            }
+        );
+  }
+
+  // try to login
+
+  // if authorized store response body
+    // redirect to home page
+
+  // else inform user for wrong credentials
 }
