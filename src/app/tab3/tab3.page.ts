a853889b621b1a68e03bdae5d189fdb2a678a55c
nikos.skl <nikos.skl98@gmail.com>
2019-09-23 23:15:47 +0300

    Edit screen's title

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Student} from '../shared/models/student.model';
+import {StorageService} from '../shared/services/storage.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  public student: Student;
+
+  constructor(
+      private storageService: StorageService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadStudentInfo();
+  }
+
+  loadStudentInfo() {
+    this.storageService.getStudent().then((student) => {
+      this.student = student;
+    });
+  }
 
 }
