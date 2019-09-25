442025b878531408aa80b09fb41ac8bec60a6ddd
nikos.skl <nikos.skl98@gmail.com>
2019-09-25 15:56:50 +0300

    Update offline-login page

--- src/app/offline-login/offline-login.page.ts
+++ src/app/offline-login/offline-login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../shared/services/auth.service';
+import {StorageService} from '../shared/services/storage.service';
+import {Student} from '../shared/models/student.model';
 
 @Component({
   selector: 'app-offline-login',
@@ -9,12 +11,20 @@ import {AuthService} from '../shared/services/auth.service';
 })
 export class OfflineLoginPage implements OnInit {
 
+  private userAem: string;
+
   constructor(
       private router: Router,
       private authService: AuthService,
+      private storageService: StorageService
   ) { }
 
   ngOnInit() {
+    this.storageService.getStudent().then((student: Student) => {
+      if (student != null) {
+        this.userAem = student.info.aem;
+      }
+    });
   }
 
   proceedOffline() {
