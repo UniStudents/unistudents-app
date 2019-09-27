87d7a4e8b7dec644255bc0cd5256227a1c8b5e62
nikos.skl <nikos.skl98@gmail.com>
2019-09-27 14:45:39 +0300

    Add logout function

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Student} from '../shared/models/student.model';
 import {StorageService} from '../shared/services/storage.service';
+import {AlertController} from '@ionic/angular';
+import {AuthService} from '../shared/services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
@@ -12,7 +15,10 @@ export class Tab3Page implements OnInit {
   public student: Student;
 
   constructor(
-      private storageService: StorageService
+      private storageService: StorageService,
+      private authService: AuthService,
+      private router: Router,
+      public alertController: AlertController
   ) {}
 
   ngOnInit(): void {
@@ -23,6 +29,34 @@ export class Tab3Page implements OnInit {
     this.storageService.getStudent().then((student) => {
       this.student = student;
     });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Αποσύνδεση',
+      message: 'Τα δεδομένα σου θα διαγραφτούν απο τη συσκευή!',
+      buttons: [
+        {
+          text: 'ΑΚΥΡΟ',
+          role: 'cancel',
+          handler: () => {
+            alert.dismiss();
+          }
+        }, {
+          text: 'ΝΑΙ',
+          handler: () => {
+            this.logout();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
