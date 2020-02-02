fec7157d68323b98b569330789ef0b8a11578a4d
nikos.skl <nikos.skl98@gmail.com>
2020-02-03 00:04:20 +0200

    Add rememberMe functionality

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,13 +1,13 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {AuthService} from '../shared/services/auth.service';
-import {IonLabel, LoadingController, ToastController} from '@ionic/angular';
-import {Router} from '@angular/router';
-import {StorageService} from '../shared/services/storage.service';
-import {Student} from '../shared/models/student.model';
-import {AppMinimize} from '@ionic-native/app-minimize/ngx';
-import {ApiService} from '../shared/services/api.service';
-import {Grades} from '../shared/models/grades.model';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { AlertController, IonLabel, LoadingController, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { StorageService } from '../shared/services/storage.service';
+import { Student } from '../shared/models/student.model';
+import { AppMinimize } from '@ionic-native/app-minimize/ngx';
+import { ApiService } from '../shared/services/api.service';
+import { Grades } from '../shared/models/grades.model';
 
 @Component({
   selector: 'app-login',
@@ -23,6 +23,8 @@ export class LoginPage implements OnInit {
   passwordType = 'password';
   passwordIcon = 'eye-off';
 
+  rememberMeOption: any;
+
   constructor(
       private router: Router,
       private storageService: StorageService,
@@ -30,27 +32,76 @@ export class LoginPage implements OnInit {
       public loadingController: LoadingController,
       private appMinimize: AppMinimize,
       private apiService: ApiService,
-      public toastController: ToastController
+      public toastController: ToastController,
+      public alertController: AlertController
   ) { }
 
   ngOnInit() {
-      this.storageService.getUsername().then((username) => {
-          this.usernameField = username;
+      this.setUsernameField();
+
+      this.getRememberMeOption().then(rememberMe => {
+          if (rememberMe === null) {
+              this.rememberMeOption = 'null';
+          } else if (rememberMe === 'true') {
+              this.rememberMeOption = 'true';
+          } else if (rememberMe === 'false') {
+              this.rememberMeOption = 'false';
+          }
+
+          if (this.rememberMeOption === 'true') {
+              this.setPasswordField();
+          }
       });
+  }
+
+  setUsernameField() {
+      this.storageService.getUsername().then((username) => {
+          if (username !== null) {
+              this.usernameField = username;
+          } else {
+              this.usernameField = '';
+          }
+      });
+  }
+
+  setPasswordField() {
+      this.storageService.getPassword().then((password) => {
+          if (password !== null) {
+              password = this.cryptoService.decrypt(password);
+              this.passwordField = password;
+          } else {
+              this.passwordField = '';
+          }
+      });
+  }
+
+  getRememberMeOption() {
+      return this.storageService.getRememberMe();
+  }
+
+  setRememberMeOption(option: string) {
+      this.storageService.saveRememberMe(option);
   }
 
   ionViewWillEnter() {
       if (this.authService.isLoggedIn) {
         this.appMinimize.minimize();
       } else {
-          this.storageService.getUsername().then((username) => {
-              if (username !== null) {
-                  this.usernameField = username;
-              } else {
-                  this.usernameField = '';
+          this.setUsernameField();
+
+          this.getRememberMeOption().then(rememberMe => {
+              if (rememberMe === null) {
+                  this.rememberMeOption = 'null';
+              } else if (rememberMe === 'true') {
+                  this.rememberMeOption = 'true';
+              } else if (rememberMe === 'false') {
+                  this.rememberMeOption = 'false';
+              }
+
+              if (this.rememberMeOption === 'true') {
+                  this.setPasswordField();
               }
           });
-          this.passwordField = '';
       }
   }
 
@@ -71,12 +122,21 @@ export class LoginPage implements OnInit {
         // compare grades
         this.compareGrades(student.grades);
 
-        // save credentials temporary for refresh data
+        // save & store username locally
         this.apiService.username = form.value.username;
-        this.apiService.password = form.value.password;
-
-        // store username for future login
         this.storageService.saveUsername(form.value.username);
+
+        if (this.rememberMeOption === 'null') {
+            this.rememberMeAlert(form);
+        } else if (this.rememberMeOption === 'true') {
+            // encrypt, save & store password locally
+            this.apiService.password = form.value.password;
+            const password = this.cryptoService.encrypt(form.value.password);
+            this.storageService.savePassword(password);
+        } else if (this.rememberMeOption === 'false') {
+            // save temporary
+            this.apiService.password = form.value.password;
+        }
 
         // save fetched data locally & navigate to home screen
         this.storageService.saveStudent(student).then(() => {
@@ -147,4 +207,48 @@ export class LoginPage implements OnInit {
       this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
       this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
   }
+
+  async rememberMeAlert(form: NgForm) {
+      const alert = await this.alertController.create({
+          header: 'Να με θυμάσαι',
+          message: 'Θέλεις να αποθηκευτεί ο κωδικός στη συσκευή σου;',
+          buttons: [
+              {
+                  text: 'ΟΧΙ',
+                  role: 'cancel',
+                  handler: () => {
+                      this.setRememberMeOption('false');
+                      this.rememberMeOption = 'false';
+                      this.apiService.password = form.value.password;
+                      alert.dismiss();
+                  }
+              }, {
+                  text: 'ΝΑΙ',
+                  handler: () => {
+                      this.setRememberMeOption('true');
+                      this.rememberMeOption = 'true';
+                      // encrypt, save & store password locally
+                      this.apiService.password = form.value.password;
+                      const password = this.cryptoService.encrypt(form.value.password);
+                      this.storageService.savePassword(password);
+                  }
+              }
+          ]
+      });
+      await alert.present();
+  }
+
+  public showFingerprintAuthDlg() {
+        this.fingerprintOptions = {
+            title: 'Biometric Authentication', // (Android Only) | optional | Default: "<APP_NAME> Biometric Sign On"
+            subtitle: 'Coolest Plugin ever', // (Android Only) | optional | Default: null
+            description: 'Please authenticate', // optional | Default: null
+            fallbackButtonTitle: 'Use Backup', // optional | When disableBackup is false defaults to "Use Pin".
+            // When disableBackup is true defaults to "Cancel"
+            disableBackup: false  // optional | default: false
+        };
+        this.fingerAuth.show(this.fingerprintOptions)
+            .then((result1: any) => this.presentToast(result1))
+            .catch((error: any) => this.presentToast(error));
+    }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,6 +6,7 @@ import { AuthService } from '../shared/services/auth.service';
 import { Router } from '@angular/router';
 import { AppVersion } from '@ionic-native/app-version/ngx';
 import { ThemeModeService } from '../shared/services/theme-mode.service';
+import { ApiService } from '../shared/services/api.service';
 
 @Component({
   selector: 'app-tab3',
@@ -17,6 +18,7 @@ export class Tab3Page implements OnInit {
   public student: Student;
   version = '1.0.0';
   status = false;
+  rememberMe = false;
 
   constructor(
       private storageService: StorageService,
@@ -24,7 +26,9 @@ export class Tab3Page implements OnInit {
       private router: Router,
       public alertController: AlertController,
       private appVersion: AppVersion,
-      private themeMode: ThemeModeService
+      private themeMode: ThemeModeService,
+      private apiService: ApiService,
+      private toastController: ToastController
   ) {}
 
   ngOnInit(): void {
@@ -39,6 +43,11 @@ export class Tab3Page implements OnInit {
     this.storageService.getThemeMode().then(mode => {
       if (mode === 'light') { this.status = false; }
       else if (mode === 'dark') { this.status = true; }
+    });
+
+    this.storageService.getRememberMe().then(rememberMe => {
+      if (rememberMe === 'true') { this.rememberMe = true; }
+      else if (rememberMe === 'false') { this.rememberMe = false; }
     });
   }
 
@@ -78,5 +87,30 @@ export class Tab3Page implements OnInit {
 
   toggleThemeMode() {
     this.themeMode.enableDarkMode(this.status);
+  }
+
+  toggleRememberMe() {
+    if (this.rememberMe === true) {
+      this.storageService.saveRememberMe('true').then(rememberMe => {
+        // encrypt, save & store password locally
+        console.log(this.apiService.password);
+        const password = this.cryptoService.encrypt(this.apiService.password);
+        this.storageService.savePassword(password).then(result => {
+          this.presentToast('Ο κωδικός σου αποθηκεύτηκε επιτυχώς!');
+        });
+      });
+    } else {
+      this.storageService.saveRememberMe('false').then(rememberMe => {
+        this.presentToast('Ο κωδικός σου διαγράφηκε επιτυχώς!');
+      });
+    }
+  }
+
+  async presentToast(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    await toast.present();
   }
 }
