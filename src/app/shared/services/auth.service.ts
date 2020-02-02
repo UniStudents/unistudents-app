edce3743dd4d0a0398c8e2857a7fa3ac02a8ca76
nikos.skl <nikos.skl98@gmail.com>
2020-02-03 00:08:11 +0200

    Update auth service to remove stored password

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Storage} from '@ionic/storage';
-import {EnvService} from './env.service';
-import {StorageService} from './storage.service';
+import { HttpClient } from '@angular/common/http';
+import { EnvService } from './env.service';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -27,6 +26,8 @@ export class AuthService {
   logout() {
     this.storageService.removeStudent();
     this.storageService.removeUsername();
+    this.storageService.removePassword();
+    this.storageService.removeRememberMe();
     this.isLoggedIn = false;
   }
 }
