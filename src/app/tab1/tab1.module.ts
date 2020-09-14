0f6840144ce724b2594ecd1a45421b2159ea0007
Nikos Sklavounos <nikos.skl98@gmail.com>
2020-09-14 16:49:48 +0300

    Update #tab1

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -10,8 +10,11 @@ import { Tab1Page } from './tab1.page';
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{path: '', component: Tab1Page}]),
   ],
-  declarations: [Tab1Page]
+  declarations: [
+      Tab1Page
+  ],
+  providers: []
 })
 export class Tab1PageModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,40 +1,115 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
-import {GradeResults} from '../shared/models/grade-results.model';
-import {Student} from '../shared/models/student.model';
-import {StorageService} from '../shared/services/storage.service';
+import { Student } from '../shared/models/student.model';
+import { Router } from '@angular/router';
+import { ApiService } from '../shared/services/api.service';
+import { StoreService } from '../shared/services/store.service';
+import { Observable } from 'rxjs';
+import { AngularFireAnalytics } from '@angular/fire/analytics';
+import { NotificationService } from '../shared/services/notification.service';
+import { RoutingService } from '../shared/services/routing.service';
+import { NetworkService } from '../shared/services/network.service';
+import { StudentService } from '../shared/services/student.service';
+import { UniversityService } from '../shared/services/university.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit {
-  @ViewChild('lineCanvas') lineCanvas: ElementRef;
-  @ViewChild('pieCanvas') pieCanvas: ElementRef;
-  @ViewChild('doughnutCanvas') doughnutCanvas: ElementRef;
+export class Tab1Page {
+  @ViewChild('lineCanvas', {static: true}) lineCanvas: ElementRef;
+  @ViewChild('doughnutCanvas', {static: true}) doughnutCanvas: ElementRef;
 
+  initGraphs = false;
+  flag = false;
+  public student: Student = null;
   private lineChart: Chart;
-  private pieChart: Chart;
   private doughnutChart: Chart;
-  public grades: GradeResults;
-  public student: Student;
+  private studentObservable: Observable<Student[]>;
 
   constructor(
-      private storageService: StorageService
+    private apiService: ApiService,
+    private router: Router,
+    private storeService: StoreService,
+    private firebaseAnalytics: AngularFireAnalytics,
+    private notificationService: NotificationService,
+    private routingService: RoutingService,
+    private networkService: NetworkService,
+    private studentService: StudentService,
+    private universityService: UniversityService
   ) {}
 
   ngOnInit(): void {
-    this.loadStudentInfo();
+    this.studentObservable = this.storeService.students;
+
+    this.studentObservable.subscribe(res => {
+      // after login-page screen
+      if (this.apiService.fetchedData && !this.flag) {
+        setTimeout(() => {
+          this.student = res[0];
+          this.flag = true;
+          this.lineChart = this.getLineChart();
+          this.doughnutChart = this.getDoughnutChart();
+          this.initGraphs = true;
+          this.setFirebase();
+        }, 400);
+      } else {
+        // after home-page screen
+        if (res.length !== 0) {
+          this.student = res[0];
+          this.apiService.fetchedData = true;
+          this.flag = true;
+          if (this.initGraphs) {
+            this.updateLineChart();
+            this.updateDoughnutChart();
+          } else {
+            this.lineChart = this.getLineChart();
+            this.doughnutChart = this.getDoughnutChart();
+            this.initGraphs = true;
+            this.setFirebase();
+          }
+          // after subscribedTopics filled
+          // subscribe to them
+          if (this.studentService.notificationsForNewGrades) {
+            this.notificationService.subscribeToTopics();
+          }
+          this.firebaseAnalytics.logEvent('refresh_grades', {screen: 'tab1'});
+        }
+      }
+    });
+
+    // start fetching student's data
+    if (!this.apiService.fetchedData) {
+      this.fetchStudent();
+    }
   }
 
-  loadStudentInfo() {
-    this.storageService.getStudent().then((student) => {
-      this.student = student;
-      this.lineChart = this.getLineChart();
-      // this.pieChart = this.getPieChart();
-      this.doughnutChart = this.getDoughnutChart();
+  ionViewWillEnter() {
+    this.routingService.currentPage = '/app/tabs/tab1';
+  }
+
+  async fetchStudent() {
+    // print stored student to front-end
+    await this.storeService.loadStoredStudent();
+
+    // if connected fetch new data
+    const status = this.networkService.networkStatus;
+    if (status.connected === true) {
+      setTimeout(() => {
+        this.storeService.fetchStudent();
+      }, 800);
+    }
+  }
+
+  setFirebase() {
+    this.firebaseAnalytics.setUserProperties({
+      Semester: this.student.info.semester,
+      Department: this.student.info.department,
+      University: this.universityService.uni
     });
+    this.notificationService.initNotificationService();
+    this.notificationService.addTopic(this.universityService.uni);
   }
 
   getDoughnutChart(): Chart {
@@ -43,10 +118,13 @@ export class Tab1Page implements OnInit {
 
     for (const semester of this.student.grades.semesters) {
       for (const course of semester.courses) {
+        if (course.grade === '') { continue; }
         if (course.grade === '-') {
           dataset[2]++;
+          this.notificationService.addTopic(this.universityService.uni + '.' + course.id);
         } else if (Number(course.grade) < 5) {
           dataset[1]++;
+          this.notificationService.addTopic(this.universityService.uni + '.' + course.id);
         }
       }
     }
@@ -73,46 +151,6 @@ export class Tab1Page implements OnInit {
     });
   }
 
-  // getPieChart(): Chart {
-  //   const gradesQuantity: Array<number> = [0, 0, 0];
-  //
-  //   for (const semester of this.student.grades.semesters) {
-  //     for (const course of semester.courses) {
-  //       if (course.grade !== '-') {
-  //         if (Number(course.grade) > 8) {
-  //           gradesQuantity[0]++;
-  //         } else if (Number(course.grade) > 6) {
-  //           gradesQuantity[1]++;
-  //         } else if (Number(course.grade) > 4) {
-  //           gradesQuantity[2]++;
-  //         } else {
-  //           continue;
-  //         }
-  //       }
-  //     }
-  //   }
-  //
-  //   return new Chart(this.pieCanvas.nativeElement, {
-  //     type: 'pie',
-  //     data: {
-  //       labels: ['Άριστα', 'Λίαν Καλώς', 'Καλώς'],
-  //       datasets: [{
-  //         backgroundColor: [
-  //           '#657BFF',
-  //           'rgba(101,123,255,0.8)',
-  //           'rgba(101,123,255,0.6)'
-  //         ],
-  //         data: gradesQuantity
-  //       }]
-  //     },
-  //     options: {
-  //       legend: {
-  //         onClick: null
-  //       }
-  //     }
-  //   });
-  // }
-
   getLineChart(): Chart {
     const grades: Array<number> = [];
     const semesters: Array<number> = [];
@@ -120,7 +158,7 @@ export class Tab1Page implements OnInit {
     for (let i = 0; i < this.student.grades.semesters.length; i++) {
       if (this.student.grades.semesters[i].gradeAverage === '-') { continue; }
       semesters.push(this.student.grades.semesters[i].id);
-      grades.push(Number(this.student.grades.semesters[i].gradeAverage.replace('-', '')));
+      grades.push(Number(this.student.grades.semesters[i].gradeAverage.replace('-', '').replace(',', '.')));
     }
 
     return new Chart(this.lineCanvas.nativeElement, {
@@ -179,5 +217,42 @@ export class Tab1Page implements OnInit {
         }
       },
     });
+  }
+
+  updateDoughnutChart() {
+    const dataset: Array<number> = [0, 0, 0];
+    dataset[0] = Number(this.student.grades.totalPassedCourses);
+
+    for (const semester of this.student.grades.semesters) {
+      for (const course of semester.courses) {
+        if (course.grade === '') { continue; }
+        if (course.grade === '-') {
+          dataset[2]++;
+          this.notificationService.addTopic(this.universityService.uni + '.' + course.id);
+        } else if (Number(course.grade) < 5) {
+          dataset[1]++;
+          this.notificationService.addTopic(this.universityService.uni + '.' + course.id);
+        }
+      }
+    }
+
+    this.doughnutChart.data.datasets[0].data = dataset;
+    this.doughnutChart.update();
+  }
+
+  updateLineChart() {
+    const grades: Array<number> = [];
+    const semesters: Array<number> = [];
+
+    for (let i = 0; i < this.student.grades.semesters.length; i++) {
+      if (this.student.grades.semesters[i].gradeAverage === '-') { continue; }
+      semesters.push(this.student.grades.semesters[i].id);
+      grades.push(Number(this.student.grades.semesters[i].gradeAverage.replace('-', '').replace(',', '.')));
+    }
+
+    this.lineChart.data.datasets[0].data = grades;
+    this.lineChart.data.labels = semesters;
+
+    this.lineChart.update();
   }
 }
