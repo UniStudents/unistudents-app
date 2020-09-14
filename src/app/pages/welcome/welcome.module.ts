676af96e54b5246de47f8ef4ea1a036b5c48fdc1
Nikos Sklavounos <nikos.skl98@gmail.com>
2020-09-14 16:52:44 +0300

    Add welcome page

---
+++ src/app/pages/welcome/welcome.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { WelcomePageRoutingModule } from './welcome-routing.module';
+
+import { WelcomePage } from './welcome.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    WelcomePageRoutingModule
+  ],
+  declarations: [WelcomePage]
+})
+export class WelcomePageModule {}
