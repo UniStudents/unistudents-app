fb2941eb7964e347e66ac562b314fdb2287b6c55
Nikos Sklavounos <nikos.skl98@gmail.com>
2022-01-21 00:43:15 +0200

    Experimental login through WebView

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:unistudents_app/widgets/web_view_stack.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
+  var textStr = 'Empty';
 
   @override
   _LoginScreenState createState() => _LoginScreenState();
@@ -10,10 +13,38 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login screen'),
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              TextButton(
+                child: const Text('Είσοδος'),
+                onPressed: () => _navigateToWebViewLogin(context)
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Text(widget.textStr)
+                ),
+              )
+            ]
+          ),
+        ),
       ),
     );
+  }
+
+  void _navigateToWebViewLogin(BuildContext buildContext) async {
+    final result = await Navigator.of(context).push(
+      MaterialPageRoute<String>(
+        builder: (ctx) => WebViewStack(),
+        fullscreenDialog: true
+      )
+    );
+
+    setState(() {
+      widget.textStr = result!;
+    });
   }
 }
--- lib/widgets/web_view_stack.dart
+++ lib/widgets/web_view_stack.dart
@@ -1,11 +1,12 @@
-import 'dart:async';     // Add this import for Completer
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewStack extends StatefulWidget {
-  const WebViewStack({required this.controller, Key? key}) : super(key: key); // Modify
+  WebViewStack({Key? key}) : super(key: key);
 
-  final Completer<WebViewController> controller;   // Add this attribute
+  final controller = Completer<WebViewController>();
 
   @override
   State<WebViewStack> createState() => _WebViewStackState();
@@ -16,62 +17,86 @@ class _WebViewStackState extends State<WebViewStack> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        WebView(
-          initialUrl: 'https://my-studies.uoa.gr/Secr3w/connect.aspx',
-          // Add from here ...
-          onWebViewCreated: (webViewController) {
-            widget.controller.complete(webViewController);
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        leading: Container(),
+        middle: const Text('Είσοδος'),
+        trailing: GestureDetector(
+          child: const Text("Πίσω"),
+          onTap: () {
+            Navigator.pop(context);
           },
-          // ... to here.
-          onPageStarted: (url) {
-            setState(() {
-              loadingPercentage = 0;
-            });
-          },
-          onProgress: (progress) {
-            setState(() {
-              loadingPercentage = progress;
-            });
-          },
-          onPageFinished: (url) {
-            setState(() {
-              loadingPercentage = 100;
-            });
-          },
-          navigationDelegate: (navigation) {
-            final host = Uri.parse(navigation.url).host;
-            if (host.contains('youtube.com')) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(
-                    'Blocking navigation to $host',
-                  ),
-                ),
-              );
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-          javascriptMode: JavascriptMode.unrestricted,
-          javascriptChannels: _createJavascriptChannels(context),
         ),
-        if (loadingPercentage < 100)
-          LinearProgressIndicator(
-            value: loadingPercentage / 100.0,
-          ),
-      ],
+      ),
+      child: SafeArea(
+        child: Stack(
+          children: [
+            WebView(
+              initialUrl: 'https://my-studies.uoa.gr/Secr3w/connect.aspx',
+              onWebViewCreated: (webViewController) {
+                widget.controller.complete(webViewController);
+              },
+              onPageStarted: (url) {
+                setState(() {
+                  loadingPercentage = 0;
+                });
+              },
+              onProgress: (progress) {
+                setState(() {
+                  loadingPercentage = progress;
+                });
+              },
+              onPageFinished: (url) {
+                setState(() {
+                  loadingPercentage = 100;
+                });
+              },
+              navigationDelegate: _onNavigation,
+              javascriptMode: JavascriptMode.unrestricted,
+              javascriptChannels: _createJavascriptChannels(context),
+            ),
+            if (loadingPercentage < 100)
+              LinearProgressIndicator(
+                value: loadingPercentage / 100.0,
+              ),
+          ],
+        ),
+      ),
     );
+  }
+
+  FutureOr<NavigationDecision> _onNavigation(navigation) async {
+    final url = Uri.parse(navigation.url).toString();
+    final controller = await widget.controller.future;
+    if (url.endsWith('https://my-studies.uoa.gr/Secr3w/app/')) {
+      controller.loadUrl('https://my-studies.uoa.gr/Secr3w/app/accHistory/default.aspx');
+      return NavigationDecision.navigate;
+    }
+    if (url.endsWith('https://my-studies.uoa.gr/Secr3w/app/accHistory/default.aspx')) {
+      await controller.runJavascript('''
+        var result = '';
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", 'https://my-studies.uoa.gr/Secr3w/app/accHistory/accadFooter.aspx?', false );
+        xmlHttp.send( null );
+        result = 'FIRST_RESPONSE: ' + xmlHttp.responseText;
+        
+        xmlHttp.open( "GET", 'https://my-studies.uoa.gr/Secr3w/app/userprofile/generalInfo.aspx', false );
+        xmlHttp.send( null );
+        result = result + ' SECONST_RESPONSE: ' + xmlHttp.responseText;
+        LoginResponse.postMessage('responseStatus: 200 | ' + result);
+      ''');
+    }
+    return NavigationDecision.navigate;
   }
 
   Set<JavascriptChannel> _createJavascriptChannels(BuildContext context) {
     return {
       JavascriptChannel(
-        name: 'SnackBar',
+        name: 'LoginResponse',
         onMessageReceived: (message) {
-          ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text(message.message)));
+          if (message.message.contains('responseStatus: 200')) {
+            Navigator.of(context).pop(message.message);
+          }
         },
       ),
     };
