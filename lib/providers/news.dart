36d48a9de6251c211902d7c79567f222ded2b529
Nikos Sklavounos <nikos.skl98@gmail.com>
2022-01-28 00:16:01 +0200

    Refactor news provider

--- lib/providers/news.dart
+++ lib/providers/news.dart
@@ -8,47 +8,12 @@ import 'package:flutter/foundation.dart';
 enum ArticlesState { fetch, refresh, old }
 
 class News with ChangeNotifier {
-  int _pageNumber = 0;
-  int _pageLimit = 10;
-
-  List<Article> _articles = [
-    Article('', 'link', [Attachment("Image", 'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg', 'img')],
-        ["Ανακοινώσεις", "Νέα"], "content", {}, "2022-01-16 22:12:00", "Πρόγραμμα σπουδών", "unipi.gr")
-  ];
-
-  List<Article> _backupArticles = [];
-  List<String> _latestIds = [];
-  List<String> _oldestIds = [];
-
-  List<NewsWebsite> _availableWebsites = [
-    NewsWebsite("unipi.gr", "Πανεπιστήμιο Πειραιώς", "/assets/unipi-logo.png", [
-      NewsWebsiteChild("unipi.gr", "Πανεπιστήμιο Πειραιά"),
-      NewsWebsiteChild("ds.unipi.gr", "Ψηφιακά Συστήματα"),
-      NewsWebsiteChild("oik.unipi.gr", "Οικονομικό Τμήμα")
-    ]),
-    NewsWebsite("uoc.gr", "Πανεπιστήμιο Κρήτης", "/assets/uoc-logo.png", [
-      NewsWebsiteChild("uoc.gr", "Πανεπιστήμιο Πειραιά"),
-      NewsWebsiteChild("ds.uoc.gr", "Ψηφιακά Συστήματα"),
-      NewsWebsiteChild("oik.uoc.gr", "Οικονομικό Τμήμα")
-    ]),
-    NewsWebsite("tuc.gr", "Πολυτεχνίο Κρήτης", "/assets/tuc-logo.png", [
-      NewsWebsiteChild("tuc.gr", "Πανεπιστήμιο Πειραιά"),
-      NewsWebsiteChild("ds.tuc.gr", "Ψηφιακά Συστήματα"),
-      NewsWebsiteChild("oik.tuc.gr", "Οικονομικό Τμήμα")
-    ])
-  ];
+  List<Article> _latestArticles = [];
+  List<NewsWebsite> _availableWebsites = [];
   List<String> _followedWebsites = [];
 
-  List<Article> get articles => [..._articles];
-
-  List<Article> get backupArticles => [..._backupArticles];
-
-  List<String> get latestIds => [..._latestIds];
-
-  List<String> get oldestIds => [..._oldestIds];
-
+  List<Article> get latestArticles => [..._latestArticles];
   List<NewsWebsite> get availableWebsites => [..._availableWebsites];
-
   List<String> get followedWebsites => [..._followedWebsites];
 
   set followedWebsites(List<String> value) {
@@ -71,31 +36,59 @@ class News with ChangeNotifier {
     }
   }
 
-  Future<void> fetchArticles({ArticlesState state = ArticlesState.fetch, List<String> filteredWebsites = const <String>[]}) async {
+  Future<List<Article>> fetchArticles({
+    List<String> filteredWebsites = const <String>[],
+    required int pageLimit
+  }) async {
     Response response;
     var websites = (filteredWebsites.isEmpty)
-      ? _followedWebsites
-      : filteredWebsites;
+        ? [..._followedWebsites]
+        : [...filteredWebsites];
 
-    switch (state) {
-      case ArticlesState.fetch:
-        response = await API.getArticles(websites,
-            pageSize: _pageLimit, pageNumber: _pageNumber);
-        break;
-      case ArticlesState.refresh:
-        response =
-            await API.getArticles(websites, afterIds: _latestIds);
-        break;
-      case ArticlesState.old:
-        response = await API.getArticles(websites,
-            pageSize: _pageLimit, beforeIds: _oldestIds);
-        break;
-    }
+    response = await API.getArticles(websites, pageSize: pageLimit, pageNumber: 0);
 
-    if (response.statusCode != 200) return;
+    if (response.statusCode != 200) throw Exception(response.statusCode);
 
-    _articles = Article.parseFromRequest(response.body).toList();
-    notifyListeners();
+    return Article.parseFromRequest(response.body).toList();
+  }
+
+  Future<List<Article>> fetchOldArticles({
+    List<String> filteredWebsites = const <String>[],
+    required List<String> oldestArticlesIds,
+    required int pageLimit
+  }) async {
+    Response response;
+    var websites = (filteredWebsites.isEmpty)
+        ? [..._followedWebsites]
+        : [...filteredWebsites];
+
+    response = await API.getArticles(websites,
+        pageSize: pageLimit,
+        pageNumber: 0,
+        beforeIds: oldestArticlesIds);
+
+    if (response.statusCode != 200) throw Exception(response.statusCode);
+
+    return Article.parseFromRequest(response.body).toList();
+  }
+
+  Future<List<Article>> fetchNewArticles({
+    List<String> filteredWebsites = const <String>[],
+    required List<String> latestArticlesIds
+  }) async {
+    Response response;
+    var websites = (filteredWebsites.isEmpty)
+        ? [..._followedWebsites]
+        : [...filteredWebsites];
+
+    response = await API.getArticles(websites,
+        pageSize: 0,
+        pageNumber: 0,
+        afterIds: latestArticlesIds);
+
+    if (response.statusCode != 200) throw Exception(response.statusCode);
+
+    return Article.parseFromRequest(response.body).toList();
   }
 
   Future<void> fetchWebsites(String university) async {
