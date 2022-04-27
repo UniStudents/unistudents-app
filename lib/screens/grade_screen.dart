8ced1e85aad65a8d4ba32a463365afa36da2d99f
George Andredakis <george@andredakis.gr>
2022-04-27 16:19:42 +0300

    Hide grade history on empty history

--- lib/screens/grade_screen.dart
+++ lib/screens/grade_screen.dart
@@ -5,7 +5,6 @@ import 'package:unistudents_app/core/colors.dart';
 import 'package:unistudents_app/core/local/locals.dart';
 import 'package:unistudents_app/models/course.dart';
 import 'package:unistudents_app/models/exam_grade.dart';
-import 'package:unistudents_app/widgets/web_view_stack.dart';
 
 class GradeScreen extends StatefulWidget {
   static const String id = 'grade_screen';
@@ -22,8 +21,8 @@ class _GradeScreenState extends State<GradeScreen> {
     var _isDarkMode = Theme.of(context).brightness == Brightness.dark;
 
     var _grade = ExamGrade(6.0, false, "6.0", "Χειμερινή", "2021-2022", "Χειμερινή");
-    var course = Course("1", "N1-1010", "Μαθηματική Ανάλυση ΙΙ", "Υποχρεωτικό", "stream", "instructor",
-        6, "6", 0, "0", 1.2, false, true, _grade, [_grade, _grade, _grade, _grade], []);
+    var course = Course("1", "N1-1010", "Μαθηματική Ανάλυση ΙΙ", "Υποχρεωτικό", "stream", "instructor", 6, "6", 0, "0",
+        1.2, false, true, _grade, [_grade, _grade, _grade, _grade], []);
 
     return Scaffold(
       body: ListView(
@@ -62,11 +61,7 @@ class _GradeScreenState extends State<GradeScreen> {
           // Circle
           Padding(
             padding: EdgeInsets.only(right: 127.w, left: 127.w),
-            child: Container(
-                width: 172.w,
-                height: 172.h,
-                color: const Color(0xFF000000)
-            ),
+            child: Container(width: 172.w, height: 172.h, color: const Color(0xFF000000)),
           ),
 
           Padding(padding: EdgeInsets.all(25.h)),
@@ -90,16 +85,12 @@ class _GradeScreenState extends State<GradeScreen> {
                               style: TextStyle(
                                   fontSize: 29.sp,
                                   color: UniColors.getUniBlue(_isDarkMode),
-                                  fontWeight: FontWeight.w700
-                              ),
+                                  fontWeight: FontWeight.w700),
                             ),
                             Padding(padding: EdgeInsets.only(top: 10.h)),
                             Text(
                               Locals.of(context)!.gradeECTs,
-                              style: TextStyle(
-                                  fontSize: 14.sp,
-                                  color: UniColors.getTextHalf(_isDarkMode)
-                              ),
+                              style: TextStyle(fontSize: 14.sp, color: UniColors.getTextHalf(_isDarkMode)),
                             )
                           ],
                         ),
@@ -114,26 +105,19 @@ class _GradeScreenState extends State<GradeScreen> {
                               style: TextStyle(
                                   fontSize: 29.sp,
                                   color: UniColors.getUniBlue(_isDarkMode),
-                                  fontWeight: FontWeight.w700
-                              ),
+                                  fontWeight: FontWeight.w700),
                             ),
                             Padding(padding: EdgeInsets.only(top: 10.h)),
                             Text(
                               Locals.of(context)!.gradeWeight,
-                              style: TextStyle(
-                                  fontSize: 14.sp,
-                                  color: UniColors.getTextHalf(_isDarkMode)
-                              ),
+                              style: TextStyle(fontSize: 14.sp, color: UniColors.getTextHalf(_isDarkMode)),
                             )
                           ],
                         ),
                       ],
                     ),
-                  )
-              ),
-
+                  )),
               Padding(padding: EdgeInsets.all(5.w)),
-
               Card(
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20.r),
@@ -150,8 +134,7 @@ class _GradeScreenState extends State<GradeScreen> {
                               style: TextStyle(
                                   fontSize: 29.sp,
                                   color: UniColors.getUniBlue(_isDarkMode),
-                                  fontWeight: FontWeight.w700
-                              ),
+                                  fontWeight: FontWeight.w700),
                             ),
                             Padding(padding: EdgeInsets.only(top: 5.h)),
                             Text(
@@ -176,142 +159,132 @@ class _GradeScreenState extends State<GradeScreen> {
                               style: TextStyle(
                                   fontSize: 29.sp,
                                   color: UniColors.getUniBlue(_isDarkMode),
-                                  fontWeight: FontWeight.w700
-                              ),
+                                  fontWeight: FontWeight.w700),
                             ),
                             Padding(padding: EdgeInsets.only(top: 5.h)),
                             Text(
                               Locals.of(context)!.gradeDepartmentAverage,
                               textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontSize: 14.sp,
-                                  color: UniColors.getTextHalf(_isDarkMode)
-                              ),
+                              style: TextStyle(fontSize: 14.sp, color: UniColors.getTextHalf(_isDarkMode)),
                             )
                           ],
                         ),
                       ],
                     ),
-                  )
-              ),
+                  )),
             ],
           ),
 
           Padding(padding: EdgeInsets.only(top: 33.h)),
 
-          // TODO - Hide history on empty
           // History
-          Text(
-            Locals.of(context)!.gradeGradeHistory,
-            style: TextStyle(
-              fontSize: 15,
-              color: UniColors.getTextHalf(_isDarkMode),
-              fontWeight: FontWeight.w700
-            ),
-          ),
-          Padding(padding: EdgeInsets.only(top: 10.h)),
-          Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.r),
-              ),
-              child: Padding(
-                padding: EdgeInsets.only(left: 24.w, right: 25.w, top: 18.h, bottom: 28.h),
-                child: Column(
-                  children: [
-                    // Title
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 135.w,
-                          child: Text(
-                            Locals.of(context)!.gradePeriod,
-                            style: TextStyle(
-                                fontSize: 15.sp,
-                                fontWeight: FontWeight.w700,
-                                color: UniColors.getTextHalf(_isDarkMode)
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 140.w,
-                          child: Text(
-                            Locals.of(context)!.gradeYear,
-                            style: TextStyle(
-                                fontSize: 15.sp,
-                                fontWeight: FontWeight.w700,
-                                color: UniColors.getTextHalf(_isDarkMode)
-                            ),
-                          ),
-                        ),
-                        Text(
-                          Locals.of(context)!.gradeGrade,
-                          style: TextStyle(
-                              fontSize: 15.sp,
-                              fontWeight: FontWeight.w700,
-                              color: UniColors.getTextHalf(_isDarkMode)
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    Padding(padding: EdgeInsets.only(top: 12.h)),
-
-                    // Data
-                    ListView.builder(
-                        itemCount: course.examGradeHistory.length,
-                        shrinkWrap: true,
-                        itemBuilder: (ctx, i) {
-                          double paddBottom = i - 1 == course.examGradeHistory.length
-                              ? 0 : 18.h;
-
-                          return Padding(
-                            padding: EdgeInsets.only(bottom: paddBottom),
-                            child: Row(
-                              children: [
-                                SizedBox(
-                                  width: 135.w,
-                                  child: Text(
-                                    course.examGradeHistory[i].displayPeriod,
-                                    style: TextStyle(
-                                        fontSize: 14.sp,
-                                        fontWeight: FontWeight.w500,
-                                        color: UniColors.getText1(_isDarkMode)
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 140.w,
-                                  child: Text(
-                                    course.examGradeHistory[i].academicYear,
-                                    style: TextStyle(
-                                        fontSize: 14.sp,
-                                        fontWeight: FontWeight.w500,
-                                        color: UniColors.getText1(_isDarkMode)
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 53.w,
-                                  child: Text(
-                                    course.examGradeHistory[i].displayGrade,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        fontSize: 25.sp,
-                                        fontWeight: FontWeight.w700,
-                                        color: course.examGradeHistory[i].isPassed
-                                            ? UniColors.getUniBlue(_isDarkMode)
-                                            : UniColors.getRed()
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        }
-                    ),
-                  ],
+          Visibility(
+            visible: course.examGradeHistory.isNotEmpty,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  Locals.of(context)!.gradeGradeHistory,
+                  style:
+                      TextStyle(fontSize: 15, color: UniColors.getTextHalf(_isDarkMode), fontWeight: FontWeight.w700),
                 ),
-              )
+                Padding(padding: EdgeInsets.only(top: 10.h)),
+                Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.r),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 24.w, right: 25.w, top: 18.h, bottom: 28.h),
+                      child: Column(
+                        children: [
+                          // Title
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 135.w,
+                                child: Text(
+                                  Locals.of(context)!.gradePeriod,
+                                  style: TextStyle(
+                                      fontSize: 15.sp,
+                                      fontWeight: FontWeight.w700,
+                                      color: UniColors.getTextHalf(_isDarkMode)),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 140.w,
+                                child: Text(
+                                  Locals.of(context)!.gradeYear,
+                                  style: TextStyle(
+                                      fontSize: 15.sp,
+                                      fontWeight: FontWeight.w700,
+                                      color: UniColors.getTextHalf(_isDarkMode)),
+                                ),
+                              ),
+                              Text(
+                                Locals.of(context)!.gradeGrade,
+                                style: TextStyle(
+                                    fontSize: 15.sp,
+                                    fontWeight: FontWeight.w700,
+                                    color: UniColors.getTextHalf(_isDarkMode)),
+                              ),
+                            ],
+                          ),
+
+                          Padding(padding: EdgeInsets.only(top: 12.h)),
+
+                          // Data
+                          ListView.builder(
+                              itemCount: course.examGradeHistory.length,
+                              shrinkWrap: true,
+                              itemBuilder: (ctx, i) {
+                                double paddBottom = i - 1 == course.examGradeHistory.length ? 0 : 18.h;
+
+                                return Padding(
+                                  padding: EdgeInsets.only(bottom: paddBottom),
+                                  child: Row(
+                                    children: [
+                                      SizedBox(
+                                        width: 135.w,
+                                        child: Text(
+                                          course.examGradeHistory[i].displayPeriod,
+                                          style: TextStyle(
+                                              fontSize: 14.sp,
+                                              fontWeight: FontWeight.w500,
+                                              color: UniColors.getText1(_isDarkMode)),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 140.w,
+                                        child: Text(
+                                          course.examGradeHistory[i].academicYear,
+                                          style: TextStyle(
+                                              fontSize: 14.sp,
+                                              fontWeight: FontWeight.w500,
+                                              color: UniColors.getText1(_isDarkMode)),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 53.w,
+                                        child: Text(
+                                          course.examGradeHistory[i].displayGrade,
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              fontSize: 25.sp,
+                                              fontWeight: FontWeight.w700,
+                                              color: course.examGradeHistory[i].isPassed
+                                                  ? UniColors.getUniBlue(_isDarkMode)
+                                                  : UniColors.getRed()),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }),
+                        ],
+                      ),
+                    )),
+              ],
+            ),
           ),
 
           Padding(padding: EdgeInsets.only(top: 22.h)),
@@ -377,30 +350,28 @@ class _GradeScreenState extends State<GradeScreen> {
 
           // Linked
           Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.r),
-            ),
-            child: Padding(
-              padding: EdgeInsets.only(top: 20.h, bottom: 20.h, left: 24.w, right: 40.w),
-              child: Row(
-                children: [
-                  Flexible(
-                    fit: FlexFit.tight,
-                    child: Text(
-                      Locals.of(context)!.gradeChains,
-                      style: TextStyle(
-                        fontSize: 14.sp,
-                        color: UniColors.getText1(_isDarkMode),
-                        fontWeight: FontWeight.w700,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.r),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(top: 20.h, bottom: 20.h, left: 24.w, right: 40.w),
+                child: Row(
+                  children: [
+                    Flexible(
+                      fit: FlexFit.tight,
+                      child: Text(
+                        Locals.of(context)!.gradeChains,
+                        style: TextStyle(
+                          fontSize: 14.sp,
+                          color: UniColors.getText1(_isDarkMode),
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
                     ),
-                  ),
-
-                  const Icon(Icons.keyboard_arrow_down_outlined)
-                ],
-              ),
-            )
-          ),
+                    const Icon(Icons.keyboard_arrow_down_outlined)
+                  ],
+                ),
+              )),
 
           Padding(padding: EdgeInsets.only(top: 22.h)),
 
@@ -424,15 +395,13 @@ class _GradeScreenState extends State<GradeScreen> {
                         ),
                       ),
                     ),
-
                     Icon(
                       Icons.star,
                       color: UniColors.getUniBlue(_isDarkMode),
                     )
                   ],
                 ),
-              )
-          ),
+              )),
 
           Padding(padding: EdgeInsets.only(top: 22.h)),
 
@@ -456,18 +425,15 @@ class _GradeScreenState extends State<GradeScreen> {
                         ),
                       ),
                     ),
-
                     Icon(
                       Icons.arrow_forward_ios,
                       color: UniColors.getRed(),
                     ),
                   ],
                 ),
-              )
-          ),
+              )),
         ],
       ),
     );
   }
-
 }
